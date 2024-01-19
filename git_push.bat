74cea33c3b55760dfea79545976eec05394d2451
saifaskri <saif.askri@outlook.de>
2024-01-19 10:14:20 +0100

    Automated commit - 19/01/2024 10:14:19,98

--- git_push.bat
+++ git_push.bat
@@ -1,13 +1,21 @@
 @echo off
 setlocal
 
+REM Set the branch name
+set "branch_name=dailycommit"
+
+REM Your commit message with timestamp
 set "commit_message=Automated commit - %date% %time%"
 
+REM Add all changes to the staging area
 git add .
 
+REM Commit changes with the generated message
 git commit -m "%commit_message%"
 
-git push origin main  
+REM Push changes to the remote repository
+git push origin %branch_name%
 
 echo Changes committed and pushed successfully.
 pause
+
